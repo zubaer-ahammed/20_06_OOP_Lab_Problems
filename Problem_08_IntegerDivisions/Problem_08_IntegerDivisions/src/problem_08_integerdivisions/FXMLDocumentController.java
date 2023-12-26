d7821adb9fb1efbf8f63c54e9753010e083a194e
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2023-12-27 01:38:16 +0600

    Problem 08 - Integer Visions (seperately coded with JavaFX and Swing)

---
+++ Problem_08_IntegerDivisions/Problem_08_IntegerDivisions/src/problem_08_integerdivisions/FXMLDocumentController.java
@@ -0,0 +1,66 @@
+package problem_08_integerdivisions;
+
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+
+/**
+ *
+ * @author zubaer
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private TextField number1;
+    @FXML
+    private TextField number2;
+    @FXML
+    private TextField result;
+   
+    
+    @FXML
+    private void handleDevideButtonAction(ActionEvent event) {
+        System.out.println("Divide!");
+        
+        try {
+            int num1 = Integer.parseInt(number1.getText());
+            int num2 = Integer.parseInt(number2.getText());      
+            
+            try {
+                double calculation_result = num1 / num2;
+                result.setText(String.valueOf( calculation_result) );
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, e.getMessage(), ButtonType.OK);
+                alert.setTitle("ArithmeticException");
+                alert.setHeaderText("Division by Zero");
+                //alert.setContentText("Error Message: "+e.getMessage());
+                alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                alert.show();
+            }
+            
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, e.getMessage(), ButtonType.OK);
+            alert.setTitle("NumberFormatException");
+            alert.setHeaderText("NumberFormatException");
+            alert.setContentText("Either Number 1 or Number 2 were not an integer");
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
+        }
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ Problem_08_IntegerDivisionsUsingSwing/src/problem_08_integerdivisionsusingswing/Problem_08_IntegerDivisionsUsingSwing.java
@@ -0,0 +1,16 @@
+package problem_08_integerdivisionsusingswing;
+
+/**
+ *
+ * @author zubaer
+ */
+public class Problem_08_IntegerDivisionsUsingSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
